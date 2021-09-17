de8f269fd7cd9dfbee12b7464fa9a3b59b20cc49
Nag <nagcloudlab@gmail.com>
2021-09-17 17:09:13 +0530

    add Nag

--- backend-service/src/main/java/com/example/BackendServiceApplication.java
+++ backend-service/src/main/java/com/example/BackendServiceApplication.java
@@ -3,6 +3,8 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Nag
+
 @SpringBootApplication
 public class BackendServiceApplication {
 
