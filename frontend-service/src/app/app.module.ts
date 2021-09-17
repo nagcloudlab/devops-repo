17e8dce549de88d2bbbe21348ed9774e8d8d2d85
Nag <nagcloudlab@gmail.com>
2021-09-17 17:37:31 +0530

    demo component

--- frontend-service/src/app/app.module.ts
+++ frontend-service/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { BatchStudentListComponent } from './batch-student-list/batch-student-list.component';
+import { DemoComponent } from './demo/demo.component';
  
 @NgModule({
   declarations: [
     AppComponent,
-    BatchStudentListComponent
+    BatchStudentListComponent,
+    DemoComponent
   ],
   imports: [
     BrowserModule,
